648810de849e1090343dcf6a93347f137ac49d45
Kashifshafati <shafatkashif@gmail.com>
2022-07-13 03:28:13 -0700

    first commit

--- test.js
+++ test.js
@@ -1,29 +1,73 @@
-let count = 0;
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
 
+const prevbtn = document.querySelector(".prev-btn");
+const nextbtn = document.querySelector(".next-btn");
+const randombtn = document.querySelector(".random-btn");
 
-btns.forEach(function(btn){
-    btn.addEventListener("click",function(e){
-        let styles = e.currentTarget.classList;
-        console.log(styles)
+let currentItem = 0;
 
-        if(styles.contains("decrease")){
-            count--
-        }
-        else if(styles.contains("increase")){
-            count++;
-        }else{
-            count=0
-        }
-      if(count < 0){
-        value.style.color="red"
-      }if(count > 0){
-        value.style.color ="green"
-      }
-      if(count === 0){
-        value.style.color="black"
-      }
-        value.textContent=count
-    })
+window.addEventListener("DOMContentLoaded", function () {
+    personList()
+});
+
+function personList(){
+    const item = reviews[currentItem];
+    img.src=item.img;
+    author.textContent=item.name;
+    job.textContent=item.job;
+    info.textContent=item.info;
+
+}
+
+nextbtn.addEventListener("click", function(){
+    currentItem++;
+    if(currentItem > reviews.length -1){
+        currentItem=0
+    }
+    personList(currentItem)
+})
+prevbtn.addEventListener("click", function(){
+    currentItem--;
+    if(currentItem < 0){
+        currentItem=reviews.length -1
+    }
+    personList(currentItem)
+})
+
+randombtn.addEventListener("click",function(){
+    currentItem=Math.floor(Math.random()*reviews.length)
+    personList(currentItem)
 })
