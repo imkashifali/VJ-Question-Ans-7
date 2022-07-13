8db4926eceb1a5a3479d80b6d88db8fab92aba25
Kashifshafati <shafatkashif@gmail.com>
2022-07-13 05:09:33 -0700

    first commit

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sideBar = document.querySelector(".sidebar");
+const modelBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btnss");
 
-
-toggleBtn.addEventListener("click",function(){
-    // console.log(sideBar.classList);
-    // console.log("listing",sideBar.classList);
-
-    // if(sideBar.classList.contains("sidebar")){
-    //     sideBar.classList.remove("sidebar")
-    // }else{
-    //     sideBar.classList.add("sidebar")
-    // }
-    sideBar.classList.toggle("sidebar")
-
-})
-
+modelBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+  modal.classList.remove("open-modal");
+});
